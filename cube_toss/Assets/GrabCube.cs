579d8330c052511c9e94399311d39a982a72d6e5
Janakitti <janakittiratana@gmail.com>
2021-06-05 16:22:56 -0400

    Add throwing capability

--- cube_toss/Assets/GrabCube.cs
+++ cube_toss/Assets/GrabCube.cs
@@ -7,11 +7,17 @@ public class GrabCube : MonoBehaviour {
     private Vector3 mOffset;
     private float mZCoord;
 
+    private Vector3 prevGrabPos;
+
+    private bool isGrabbed;
+
+    /*
     private void OnMouseDown()
     {
         mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
         mOffset = gameObject.transform.position - GetMouseWorldPos();
-    }
+        
+    }*/
 
     private Vector3 GetMouseWorldPos()
     {
@@ -22,10 +28,18 @@ public class GrabCube : MonoBehaviour {
         return Camera.main.ScreenToWorldPoint(mousePoint);
     }
 
+    /*
     private void OnMouseDrag()
     {
         transform.position = GetMouseWorldPos() + mOffset;
     }
+
+    private void OnMouseUp()
+    {
+        Vector3 endPos = gameObject.transform.position - prevGrabPos;
+        float speed = (gameObject.transform.position - prevGrabPos).magnitude / Time.deltaTime;
+        gameObject.GetComponent<Rigidbody>().velocity = speed * endPos.normalized;
+    }*/
 
     // Use this for initialization
     void Start () {
@@ -34,6 +48,32 @@ public class GrabCube : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isGrabbed)
+            {
+
+                isGrabbed = false;
+                Vector3 endPos = gameObject.transform.position - prevGrabPos;
+                float speed = (gameObject.transform.position - prevGrabPos).magnitude / Time.deltaTime;
+                gameObject.GetComponent<Rigidbody>().velocity = speed * endPos.normalized;
+            } else
+            {
+                isGrabbed = true;
+                mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+                mOffset = gameObject.transform.position - GetMouseWorldPos();
+            }
+        } 
+        if (isGrabbed)
+        {
+
+            prevGrabPos = gameObject.transform.position;
+
+            transform.position = GetMouseWorldPos() + mOffset;
+        }
+        
+       
+
+    }
 }
--- cube_toss/Assets/Scripts/TossCube.cs
+++ cube_toss/Assets/Scripts/TossCube.cs
@@ -16,25 +16,30 @@ public class TossCube : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown(1))
+		if (Input.GetMouseButtonDown(0))
         {
+            Debug.Log("AYOOO");
             if (grabbedObject == null)
             {
+                Debug.Log("Try Grab");
                 TryGrabObject(GetMouseOnObject(5));
             } else
             {
+                Debug.Log("Drop");
                 DropObject();
             }
         }
         if (grabbedObject != null)
         {
-            Vector3 newPosition = gameObject.transform.position + Camera.main.transform.forward * grabbedObjectSize;
+            Vector3 newPosition = gameObject.transform.position -Camera.main.transform.forward * grabbedObjectSize;
             grabbedObject.transform.position = newPosition;
+            Debug.Log(newPosition);
         }
 	}
 
     GameObject GetMouseOnObject(float range)
     {
+        
         Vector3 position = gameObject.transform.position;
         RaycastHit raycastHit;
         Vector3 target = position + Camera.main.transform.forward * range;
