4869794fe2a7e8bf5a18bd653a12d4eb82f2f034
Janakitti <janakittiratana@gmail.com>
2021-06-05 14:48:03 -0400

    Add grab functionality

---
+++ cube_toss/Assets/GrabCube.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabCube : MonoBehaviour {
+
+    private Vector3 mOffset;
+    private float mZCoord;
+
+    private void OnMouseDown()
+    {
+        Debug.Log("MOUSEDOWN");
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+
+        mousePoint.z = mZCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + mOffset;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- cube_toss/Assets/Scripts/TossCube.cs
+++ cube_toss/Assets/Scripts/TossCube.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class TossCube : MonoBehaviour {
 
+    GameObject grabbedObject;
+    float grabbedObjectSize;
+
+    
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +16,65 @@ public class TossCube : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetMouseButtonDown(1))
+        {
+            if (grabbedObject == null)
+            {
+                TryGrabObject(GetMouseOnObject(5));
+            } else
+            {
+                DropObject();
+            }
+        }
+        if (grabbedObject != null)
+        {
+            Vector3 newPosition = gameObject.transform.position + Camera.main.transform.forward * grabbedObjectSize;
+            grabbedObject.transform.position = newPosition;
+        }
 	}
+
+    GameObject GetMouseOnObject(float range)
+    {
+        Vector3 position = gameObject.transform.position;
+        RaycastHit raycastHit;
+        Vector3 target = position + Camera.main.transform.forward * range;
+
+        if (Physics.Linecast(position, target, out raycastHit))
+        {
+            return raycastHit.collider.gameObject;
+        }
+        return null;
+
+    }
+
+    void TryGrabObject(GameObject grabObject)
+    {
+        if (grabObject == null || !CanGrab(grabObject))
+        {
+            return;
+        }
+
+        grabbedObject = grabObject;
+        grabbedObjectSize = grabbedObject.GetComponent<MeshRenderer>().bounds.size.magnitude;
+    }
+
+    void DropObject()
+    {
+        if (grabbedObject == null)
+        {
+            return;
+        }
+
+        if (grabbedObject.GetComponent<Rigidbody>() != null)
+        {
+            grabbedObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+        grabbedObject = null;
+    }
+
+    bool CanGrab(GameObject cand)
+    {
+        return cand.GetComponent<Rigidbody>() != null;
+    }
+
 }
