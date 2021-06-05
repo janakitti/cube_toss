c92644ed301c2552ba7c52030ccaa7c642afd57e
Janakitti <janakittiratana@gmail.com>
2021-06-05 16:49:19 -0400

    Add selection box

---
+++ cube_toss/Assets/RectSelect.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RectSelect : MonoBehaviour {
+    public RectTransform selectionBox;
+    private Vector2 startPos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            startPos = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+
+        }
+        if (Input.GetMouseButton(0))
+        {
+            UpdateSelectionBox(Input.mousePosition);
+        }
+	}
+
+    void UpdateSelectionBox(Vector2 curMousePos)
+    {
+        if(!selectionBox.gameObject.activeInHierarchy)
+        {
+            selectionBox.gameObject.SetActive(true);
+        }
+
+        float width = curMousePos.x - startPos.x;
+        float height = curMousePos.y - startPos.y;
+
+        selectionBox.sizeDelta = new Vector2(Mathf.Abs(width), Mathf.Abs(height));
+        selectionBox.anchoredPosition = startPos + new Vector2(width / 2, height / 2);
+    }
+}
