5671234a17596c394d8edaf1e408ce51b5fb7d5e
Janakitti <janakittiratana@gmail.com>
2021-06-05 20:07:22 -0400

    Add cube generator

---
+++ cube_toss/Assets/CubeGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGenerator : MonoBehaviour {
+
+    public Rigidbody cube;
+    public float initForce = -500f;
+	
+	void Update () {
+		if (Input.GetKeyDown("c"))
+        {
+            Rigidbody instance = Instantiate(cube, new Vector3(0f, 10f, 0), new Quaternion(0, 0, 0, 0));
+            instance.AddForce(new Vector3(0, initForce, 0));
+        }
+	}
+}
