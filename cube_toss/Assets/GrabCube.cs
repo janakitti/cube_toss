171679f42ed2377f07ba00ddcc8d61e3d86ad7a2
Janakitti <janakittiratana@gmail.com>
2021-06-05 23:10:29 -0400

    Refactor GrabCube, add comments

--- cube_toss/Assets/GrabCube.cs
+++ cube_toss/Assets/GrabCube.cs
@@ -8,35 +8,44 @@ public class GrabCube : MonoBehaviour {
     public Material selectedMaterial;
     public Material multiSelectedMaterial;
 
-    private Vector3 mOffset;
-    private float mZCoord;
+    // Offset from where mouse was clicked in 3D space
+    private Vector3 mouseOffset;
+    // z coordinate of the Cube
+    private float zCube;
 
-    private Vector3 prevGrabPos;
+    // Keeps track of previous Cube position for creating a throw vector
+    private Vector3 prevCubePos;
 
+    // Indicates if a Cube being grabbed on its own
     private bool isGrabbed;
+    // Indicates if a Cube was just released
     private bool isReleased;
+
+    // Indicates if Cube is part of a multi selection
     private bool isMultiSelected;
+    // Indicates if a multi-selected Cube is being grabbed
     private bool isMultiGrabbed = false;
 
-    public void SetIsMultiSelected(bool b)
+
+    public void SetIsMultiSelected(bool isMultiSelected)
     {
-        isMultiSelected = b;
+        this.isMultiSelected = isMultiSelected;
     }
     
     private void OnMouseDown()
     {
-        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-        mOffset = gameObject.transform.position - GetMouseWorldPos();
+        // Capture z coordinate of Cube and difference between Cube position and mouse click position
+        zCube = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mouseOffset = gameObject.transform.position - CalcMouseToWorldPos();
 
         isGrabbed = true;
     }
 
-    private Vector3 GetMouseWorldPos()
+    // Calculate the point in 3D space based on mouse position on screen and z coordinate of Cube
+    private Vector3 CalcMouseToWorldPos()
     {
         Vector3 mousePoint = Input.mousePosition;
-
-        mousePoint.z = mZCoord;
-
+        mousePoint.z = zCube;
         return Camera.main.ScreenToWorldPoint(mousePoint);
     }
     
@@ -44,54 +53,57 @@ public class GrabCube : MonoBehaviour {
     {
         isReleased = true;
     }
-    
-    // Use this for initialization
-    void Start () {
-		
-	}
 
-    // Update is called once per frame
     void Update () {
         if (isMultiSelected)
         {
             GetComponent<Renderer>().material = multiSelectedMaterial;
+            // When user first clicks after multi selecting
             if (Input.GetMouseButtonDown(0))
             {
-                mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-                mOffset = gameObject.transform.position - GetMouseWorldPos();
+                zCube = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+                mouseOffset = gameObject.transform.position - CalcMouseToWorldPos();
                 isMultiGrabbed = true;
-                
             }
+            // While multi selection is being dragged
             if (Input.GetMouseButton(0))
             {
-                prevGrabPos = gameObject.transform.position;
-                transform.position = GetMouseWorldPos() + mOffset;
+                // Record current Cube position before updating
+                prevCubePos = gameObject.transform.position;
+                transform.position = CalcMouseToWorldPos() + mouseOffset;
             }
+            // When multi selection is released
             if (Input.GetMouseButtonUp(0) && isMultiGrabbed)
             {
                 GetComponent<Renderer>().material = defaultMaterial;
-                Vector3 endPos = gameObject.transform.position - prevGrabPos;
-                float speed = endPos.magnitude / Time.deltaTime;
-                gameObject.GetComponent<Rigidbody>().velocity = speed * endPos.normalized;
+                Vector3 throwDirection = gameObject.transform.position - prevCubePos;
+
+                // Divide by deltaTime to ensure speeds are not dependent on framerate
+                float speed = throwDirection.magnitude / Time.deltaTime;
+                gameObject.GetComponent<Rigidbody>().velocity = speed * throwDirection.normalized;
                 isMultiSelected = false;
                 isMultiGrabbed = false;
             }
         } else
         {
+            // When single Cube is released
             if (isReleased)
             {
-                Vector3 endPos = gameObject.transform.position - prevGrabPos;
-                float speed = endPos.magnitude / Time.deltaTime;
-                gameObject.GetComponent<Rigidbody>().velocity = speed * endPos.normalized;
+                Vector3 throwDirection = gameObject.transform.position - prevCubePos;
+
+                // Divide by deltaTime to ensure speeds are not dependent on framerate
+                float speed = throwDirection.magnitude / Time.deltaTime;
+                gameObject.GetComponent<Rigidbody>().velocity = speed * throwDirection.normalized;
                 isReleased = false;
                 isGrabbed = false;
             }
             else
             {
+                // When single Cube is being dragged
                 if (isGrabbed)
                 {
-                    prevGrabPos = gameObject.transform.position;
-                    transform.position = GetMouseWorldPos() + mOffset;
+                    prevCubePos = gameObject.transform.position;
+                    transform.position = CalcMouseToWorldPos() + mouseOffset;
                     GetComponent<Renderer>().material = selectedMaterial;
                 }
                 else
